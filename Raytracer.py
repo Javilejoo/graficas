74b60ed85eb9dbc51ac381f2d8ce56044a8d7498
Javilejo <pra21486@uvg.edu.gt>
2023-09-19 21:51:09 -0600

    Snowman
    se construyo el snowman en base a esferas y una buena iluminacion

--- Raytracer.py
+++ Raytracer.py
@@ -16,18 +16,38 @@ screen.set_alpha(None)
 raytracer = Raytracer(screen)
 raytracer.rtClearColor(0.25,0.25,0.25)
 
-brick = Material(diffuse = (1,0.4,0.4), spec = 8, ks = 0.01)
+""" brick = Material(diffuse = (1,0.4,0.4), spec = 8, ks = 0.01)
 grass = Material(diffuse = (0.4,1,0.4), spec =32, ks = 0.1) 
-water = Material(diffuse = (0.4,0.4,1), spec = 256, ks = 0.2 )
+water = Material(diffuse = (0.4,0.4,1), spec = 256, ks = 0.2 ) """
+snow = Material(diffuse= (1,1,1), spec= 0.2, ks = 0.1)
+rock = Material(diffuse=(0,0,0), spec = 3, ks = 0.1)
+carot = Material(diffuse=(1,0.5,0), spec = 0.5, ks = 0.3)
+eyes = Material(diffuse= (0.9,0.9,0.9), spec= 0.4, ks = 0.1)
 
-raytracer.scene.append(Sphere(position = (1,1,-5), radius=0.5, material = grass))
-raytracer.scene.append(Sphere(position = (0,0,-7), radius=2, material = brick))
-raytracer.scene.append(Sphere(position = (0.5,-1,-4), radius=0.3, material = water))
+#Cuerpo
+raytracer.scene.append(Sphere(position = (0,-2,-9), radius=2, material = snow))
+raytracer.scene.append(Sphere(position = (0,-1.5,-7.2), radius=0.3, material = rock))
 
+#mitad
+raytracer.scene.append(Sphere(position = (0,1,-9), radius=1.5, material = snow))
+raytracer.scene.append(Sphere(position = (0,0,-8), radius=0.3, material = rock))
+raytracer.scene.append(Sphere(position = (0,1.5,-7.7), radius=0.3, material = rock))
 
-raytracer.lights.append( AmbientLight(intensity = 0.1))
-raytracer.lights.append(DirectionalLight(direction =  (-1,-1,-1), intensity=0.7))
-raytracer.lights.append(PointLight(point = (2.5,0,-5),intensity = 0.5, color=(1,0,0)))
+#Cabeza
+raytracer.scene.append(Sphere(position = (0,3.4,-9), radius=1, material = snow))
+raytracer.scene.append(Sphere(position = (0,2.9,-7.7), radius=0.3, material = carot))
+raytracer.scene.append(Sphere(position = (-0.4,3.2,-7.7), radius=0.25, material = eyes))
+raytracer.scene.append(Sphere(position = (0.4,3.2,-7.7), radius=0.25, material = eyes))
+raytracer.scene.append(Sphere(position = (-0.4,3.2,-7.5), radius=0.1, material = rock))
+raytracer.scene.append(Sphere(position = (0.4,3.2,-7.5), radius=0.1, material = rock))
+raytracer.scene.append(Sphere(position = (0,2.2,-7.7), radius=0.1, material = rock))
+raytracer.scene.append(Sphere(position = (0.5,2.4,-7.7), radius=0.1, material = rock))
+raytracer.scene.append(Sphere(position = (-0.5,2.4,-7.7), radius=0.1, material = rock))
+
+#Luces
+raytracer.lights.append(AmbientLight(intensity=0.2, color=(1, 1, 1)))  
+raytracer.lights.append(DirectionalLight(direction=(-1, -1, -1), intensity=0.8, color=(1, 1, 1)))  
+raytracer.lights.append(PointLight(point=(0, 0, -5), intensity=0.5, color=(1, 1, 1)))  
 
 isRunning = True
 while isRunning:  
@@ -43,4 +63,3 @@ while isRunning:
     pygame.display.flip()
 
 pygame.quit()
-
